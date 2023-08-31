e2ec56a1f1ac155d15eb2b2560192ca2acd60fd5
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-08-31 19:00:11 +0200

    Add imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 
 from flask import Flask
+from flask_ckeditor import CKEditor
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
+ckeditor = CKEditor(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@127.0.0.1/quantumknn_db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "jshwifhjwieoajhf5847f5ae4eaws"
