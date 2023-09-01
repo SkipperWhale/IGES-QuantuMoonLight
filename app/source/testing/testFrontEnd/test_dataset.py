e36be12635dfe0b3019c2c721e6055dc04b19fc7
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2023-09-01 18:47:52 +0200

    fixig file path

--- app/source/testing/testFrontEnd/test_dataset.py
+++ app/source/testing/testFrontEnd/test_dataset.py
@@ -9,23 +9,40 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
+import pathlib
+from selenium.webdriver.support import expected_conditions as EC
+import time
 class TestDataset():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
-  
+    #self.driver.find_element(By.CSS_SELECTOR, ".fourth").click()
   def teardown_method(self, method):
     self.driver.quit()
-  
+
   def test_tC211(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) > label").click()
+
+    self.driver.execute_script("window.scrollTo(0, 700);")
+    time.sleep(1)
+
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(2)").click()
     self.driver.find_element(By.ID, "kFoldValue").click()
     self.driver.find_element(By.ID, "kFoldValue").send_keys("1")
@@ -34,52 +51,129 @@ class TestDataset():
   def test_tC212(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+
+
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) > label").click()
+    self.driver.execute_script("window.scrollTo(0, 700);")
+    time.sleep(1)
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(2)").click()
+
     self.driver.find_element(By.CSS_SELECTOR, ".fa-rocket").click()
   
   def test_tC221(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\test.txt")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "test.txt"))
+
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+    self.driver.execute_script("window.scrollTo(0, 700);")
+    time.sleep(1)
+
     self.driver.find_element(By.ID, "submitForm").click()
     assert self.driver.switch_to.alert.text == "Training set non inserito e/o non valido"
   
   def test_tC222(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
+
+    self.driver.execute_script("window.scrollTo(0, 700);")
+    time.sleep(1)
     self.driver.find_element(By.ID, "submitForm").click()
   
   def test_tC311(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+
+    self.driver.execute_script("window.scrollTo(0, 600);")
+    time.sleep(1)
+
+    time.sleep(1)
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) > label").click()
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(7)").click()
     self.driver.find_element(By.ID, "nrRows").send_keys("0")
+    time.sleep(1)
     self.driver.find_element(By.ID, "submitForm").click()
   
   def test_tC312(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) > label").click()
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+
+    self.driver.execute_script("window.scrollTo(0, 700);")
+    time.sleep(1)
+
     self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(7)").click()
     self.driver.find_element(By.ID, "nrRows").send_keys("11")
     self.driver.find_element(By.ID, "submitForm").click()
@@ -87,11 +181,27 @@ class TestDataset():
   def test_tC321(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) > label").click()
+
+    self.driver.execute_script("window.scrollTo(0, 800);")
+    time.sleep(1)
+
     self.driver.find_element(By.ID, "nrColumns").click()
     self.driver.find_element(By.ID, "nrColumns").send_keys("0")
     self.driver.find_element(By.ID, "submitForm").click()
@@ -99,11 +209,26 @@ class TestDataset():
   def test_tC322(self):
     self.driver.get("http://127.0.0.1:5000/")
     self.driver.set_window_size(1936, 1048)
+    self.driver.find_element(By.CSS_SELECTOR, ".user").click()
+    self.driver.find_element(By.ID, "login").click()
+    self.driver.find_element(By.ID, "login").send_keys("ADeCurtis123gmail.com")
+    self.driver.find_element(By.ID, "password").send_keys("Password123")
+    element = WebDriverWait(self.driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fourth")))
+    element.click()
+    path_datasets = pathlib.Path(__file__).resolve().parent.parent / "testingFiles"
+    self.driver.get("http://127.0.0.1:5000/")
+    time.sleep(1)
+    self.driver.set_window_size(1936, 1048)
     self.driver.find_element(By.LINK_TEXT, "QUANTUM ML").click()
     self.driver.find_element(By.ID, "inputFakeBrowseFileTrain").click()
-    self.driver.find_element(By.ID, "inputFileTrain").send_keys("C:\\fakepath\\Data_training.csv")
+    self.driver.find_element(By.ID, "inputFileTrain").send_keys(str(path_datasets / "Data_training.csv"))
+
     self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) > label").click()
     self.driver.find_element(By.LINK_TEXT, "Advanced Options").click()
+
+    self.driver.execute_script("window.scrollTo(0, 750);")
+    time.sleep(1)
+
     self.driver.find_element(By.ID, "nrColumns").click()
     self.driver.find_element(By.ID, "nrColumns").send_keys("15")
     self.driver.find_element(By.ID, "submitForm").click()
